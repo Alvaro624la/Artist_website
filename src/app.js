85c9e1b73d86a5f79ce55421b52afbf54b6239f4
Alvaro624la <alvarodepradotrabajo@gmail.com>
2022-04-13 12:36:01 +0200

    shop html css finished(responsive: no shop-navbar)

--- src/app.js
+++ src/app.js
@@ -48,37 +48,47 @@ if(window.location.pathname == '/html/lmguzman.html'){
     function next(){
         let sliderSectionFirst = sliderSection[0];
         SLIDER.style.marginLeft = '-200%';
-        SLIDER.style.transition = 'all .5s';
+        SLIDER.style.transition = 'all 1s';
         setTimeout(function(){
             SLIDER.style.transition = 'none';
             SLIDER.insertAdjacentElement('beforeend', sliderSectionFirst);
             SLIDER.style.marginLeft = '-100%';
-        }, 500);
+        }, 1000);
     };
 
     function prev(){
         let sliderSection = document.getElementsByClassName('home__main__carousel-cont__slider__section');
         let sliderSectionLast = sliderSection[sliderSection.length - 1];
         SLIDER.style.marginLeft = '0%';
-        SLIDER.style.transition = 'all .5s';
+        SLIDER.style.transition = 'all 1s';
         setTimeout(function(){
             SLIDER.style.transition = 'none';
             SLIDER.insertAdjacentElement('afterbegin', sliderSectionLast);
             SLIDER.style.marginLeft = '-100%';
-        }, 500);
+        }, 1000);
     };
 
     BTNRIGHT.addEventListener('click', function(){
         next();
+        //reiniciar intervalo al clickar en btn
+        clearInterval(autocarousel);
+        autocarousel = setInterval(function(){
+            next();
+        }, 4000);
     });
     BTNLEFT.addEventListener('click', function(){
         prev();
+        //reiniciar intervalo al clickar en btn
+        clearInterval(autocarousel);
+        autocarousel = setInterval(function(){
+            next();
+        }, 4000);
     });
 
-    //MOVIMIENTO AUTOMÁTICO
-    // setInterval(function(){
-    //     next();
-    // }, 3000);
+    //MOVIMIENTO AUTOMÁTICO al cargar pagina
+    let autocarousel = setInterval(function(){
+        next();
+    }, 4000);
 
 } else {};
 
