14dc77e050333b8696cafc2547ddb3876572af13
Alvaro624la <alvarodepradotrabajo@gmail.com>
2022-04-27 21:36:18 +0200

    JS cart - añadiendo boton addToCart

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import {objects} from "./modulos/objects.js"; //SHOP PAGE --> FILTERS(LEFT SHOP MENÚ)
+import {stockProductos} from "./modulos/stockproductos.js";
 /*
 import * as "variables" from "./modulos/variables";
 import * as "funciones" from "./modulos/funciones";
@@ -136,16 +136,6 @@ if(window.location.pathname == '/html/shop.html'){
     let cartContainer = document.getElementById('cart__main');
 
     //FUNCTIONS
-    //Cards id
-    for (var i = 0; allCard.length > i; i++) {
-        // console.log(allCard[i].id);
-
-    }
-
-    //Objets
-    for (var i = 0; objects.length > i; i++) {
-        // console.log(objects[i]);
-    }
 
     //asignar propiedades de x objeto.id a x cards.id de html. Así cada card tendrá categorias, id y demás propiedades
     //si coincide categoria de indice + categoria del card = mostrar éstos coincidentes
@@ -155,36 +145,80 @@ if(window.location.pathname == '/html/shop.html'){
     //VARIABLES
     let cardImgCont = document.getElementsByClassName('shop__main__grid-1__shop-cont__card__img-cont');
     //FUNCIONES
-    function addToCart(){
-        for(let i = 0; i <= cardImgCont.length -1; i++){
-            cardImgCont[i].addEventListener('mouseenter', (e)=>{
-                let addBtn = document.createElement('a');
-                cardImgCont[i].appendChild(addBtn);
-                addBtn.className = 'shop__main__grid-1__shop-cont__card__img-cont__add-btn';
-                addBtn.innerHTML = 'Add to cart';
+    // function addToCart(){
+    //     for(let i = 0; i <= cardImgCont.length -1; i++){
+    //         cardImgCont[i].addEventListener('mouseenter', (e)=>{
+    //             let addBtn = document.createElement('a');
+    //             cardImgCont[i].appendChild(addBtn);
+    //             addBtn.className = 'shop__main__grid-1__shop-cont__card__img-cont__add-btn';
+    //             addBtn.innerHTML = 'Add to cart';
 
-                //CART ITEMS ARRAY //https://www.youtube.com/watch?v=Mm3iLqhZB1A&ab_channel=GCode
-                let cartItems = [];
-                addBtn.addEventListener('click', (e)=>{
-                    let targetId = e.target.parentElement.parentElement.id; //find --> card id
+    //             //CART ITEMS ARRAY //
+    //             let cartItems = [];
+    //             addBtn.addEventListener('click', (e)=>{
+    //                 let targetId = e.target.parentElement.parentElement.id; //find --> card id
 
-                    // let allTargetId = document.getElementById('shop-cont');
-                    // console.log(allTargetId.childNodes);
+    //                 cartItems.push(targetId);
+    //                 console.log(cartItems);
+    //             });
 
-                    cartItems.push(targetId); // cartItems --> [targetId]
-                    // cartItems = 
-                    console.log(cartItems);
-                });
-
-                cardImgCont[i].addEventListener('mouseleave', function addCartOut(){
-                    addBtn.parentNode.removeChild(addBtn);
-                    document.removeEventListener('mouseleave', addCartOut);                    
-                });
-            });
+    //             cardImgCont[i].addEventListener('mouseleave', function addCartOut(){
+    //                 addBtn.parentNode.removeChild(addBtn);
+    //                 document.removeEventListener('mouseleave', addCartOut);                    
+    //             });
+    //         });
             
-        };
-    };
-    addToCart();
+    //     };
+    // };
+    // addToCart();
+
+    // CARRITO ONLY JAVASCRIPT (video: https://www.youtube.com/watch?v=Mm3iLqhZB1A&ab_channel=GCode)(6:51 min)
+    const contenedorProductos = document.getElementById('shop-cont');
+
+    let cart = [];
+
+    stockProductos.forEach((producto) => {
+        const li = document.createElement('li');
+            const divImg = document.createElement('div');
+                const img = document.createElement('img');
+            const divDesc = document.createElement('div');
+                const pName = document.createElement('p');
+                const pSize = document.createElement('p');
+                const pPrice = document.createElement('p');
+
+        li.classList.add('shop__main__grid-1__shop-cont__card');
+            divImg.classList.add('shop__main__grid-1__shop-cont__card__img-cont');
+            divDesc.classList.add('shop__main__grid-1__shop-cont__card__description-cont');
+                pName.classList.add('shop__main__grid-1__shop-cont__card__description-cont__name');
+                pSize.classList.add('shop__main__grid-1__shop-cont__card__description-cont__size');
+                pPrice.classList.add('shop__main__grid-1__shop-cont__card__description-cont__price');
+
+        divImg.innerHTML = `<img class='shop__main__grid-1__shop-cont__card__img-cont__img' src=${producto.img} alt="...">`;
+        pName.innerHTML = `<p>${producto.name}</p>`
+        pSize.innerHTML = `<p>${producto.size}</p>`
+        pPrice.innerHTML = `<p>${producto.price}</p>`
+        li.innerHTML = `<button id="add${producto.id}" class="shop__main__grid-1__shop-cont__card__img-cont__add-btn">Add to cart</button>`;
+        
+        contenedorProductos.appendChild(li);
+        li.appendChild(divImg);
+        li.appendChild(divDesc);
+        divDesc.appendChild(pName);
+        divDesc.appendChild(pSize);
+        divDesc.appendChild(pPrice);
+
+        const btn = document.getElementById('add${producto.id}');
+
+        btn.addEventListener('click', ()=>{
+            addToCart(producto.id);
+        });
+
+    });
+
+    const addToCart = (productId) => {
+        const item = stockProductos.find((product) => product.id === productId);
+        cart.push(item);
+        console.log(cart);
+    }
 
 
 }; //END (SHOP PAGE)
---
+++ src/modulos/stockproductos.js
@@ -0,0 +1,11 @@
+export let stockProductos = [
+    {id: '000001', name: 'monalisa', size: '120x50cm', price: '70€', category: 'models', img: '../src/img/shop/monalisa.jpeg'},
+    {id: '000002', name: 'cold', size: '100x40cm', price: '30€', category: 'animals', img: '../src/img/shop/cold.jpeg'},
+    {id: '000003', name: 'extasis', size: '300x100cm', price: '99€', category: 'oil', img: '../src/img/shop/extasis.jpeg'},
+    {id: '000004', name: 'mercredi', size: '210x100cm', price: '40€', category: 'sculptures', img: '../src/img/shop/mercredi.jpeg'},
+    {id: '000005', name: 'where', size: '130x60cm', price: '45€', category: 'landscapes', img: '../src/img/shop/where.jpeg'},
+    {id: '000006', name: 'universe', size: '120x20cm', price: '60€', category: 'oil', img: '../src/img/shop/universe.jpeg'},
+    {id: '000007', name: 'gold', size: '400x100cm', price: '59€', category: 'models', img: '../src/img/shop/gold.jpeg'},
+    {id: '000008', name: 'no longer', size: '110x50cm', price: '40€', category: 'landscapes', img: '../src/img/shop/no-longer.jpeg'},
+    {id: '000009', name: 'beauty', size: '300x100cm', price: '50€', category: 'models', img: '../src/img/shop/beauty.jpeg'}
+];
