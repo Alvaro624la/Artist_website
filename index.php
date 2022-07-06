6cf0840c124ece875412adbf86ea47c4410fe69d
Alvaro624la <alvarodepradotrabajo@gmail.com>
2022-07-06 12:55:50 +0200

    rpuebas

---
+++ index.php
@@ -0,0 +1,4 @@
+<main>
+    <h1>Título</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium natus reiciendis totam cupiditate, facere, eos consequatur quisquam velit nam libero est sequi nisi suscipit porro, a consequuntur hic asperiores.</p>
+</main>
